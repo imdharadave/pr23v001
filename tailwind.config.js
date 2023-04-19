204dda8cd286c3f6b5f56d40b27445660799a3cb
dharadave <imdavedhara@gmail.com>
2023-04-18 21:38:46 -0400

    Changes in global

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 
 
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './ui/**/*.{js,ts,jsx,tsx}'],
   theme: {
