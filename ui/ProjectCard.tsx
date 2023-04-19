d88a74bcda8cf2c47a11e893f302063a2a6dcf6c
dharadave <imdavedhara@gmail.com>
2023-04-18 22:07:31 -0400

    add styles/projectcard

--- ui/ProjectCard.tsx
+++ ui/ProjectCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
 import Manami from '@/public/Images/manami.png';
-//import '@/styles/projectcard.css';
+import '@/styles/projectcard.css';
 type Props = {};
 
 const projectdata = [
@@ -13,19 +13,19 @@ const projectdata = [
       button: 'Read More',
     },
     {
-        id: 1,
+        id: 2,
         img: 'https://images.pexels.com/photos/246753/pexels-photo-246753.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         title: 'AutoWelder 3.0',
         button: 'Read More',
       },
       {
-        id: 1,
+        id: 3,
         img: 'https://images.pexels.com/photos/6755131/pexels-photo-6755131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         title: 'AutoCutter',
         button: 'Read More',
       },
       {
-        id: 1,
+        id: 4,
         img: 'https://images.pexels.com/photos/8471981/pexels-photo-8471981.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         title: 'Data Logger/Weld Metrix',
         button: 'Read More',
@@ -35,9 +35,9 @@ const projectdata = [
 
 function ProjectCard  ({}: Props) {
   return (
-   <section className='projectcard pb-16  bg-gradient-to-r from-[#e1e7ee] to-neutral-400'>
-    <h2 className=' projectcard__title'> Our Project</h2>
-    <div className='projectcard__content'>
+   <section className="projectcard pb-16  bg-gradient-to-r from-[#e1e7ee] to-neutral-400">
+    <h2 className="projectcard__title"> Our Project</h2>
+    <div className="projectcard__content">
         {projectdata.map((data) => (
             <div className="bg-secondary3 rounded-lg">
             <Link href="#">
